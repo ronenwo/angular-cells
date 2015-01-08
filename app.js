9e8a59c6a46dfb74b5792f131bec1c9834c9d065
Ronen Wolfson <ronenwolfson@Ronens-MacBook-Pro.local>
2015-01-08 17:48:18 +0200

    two rows of cells

---
+++ app.js
@@ -0,0 +1,17 @@
+function ContactController($scope) {
+
+
+    $scope.cells1 = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    $scope.rows  = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    $scope.cells = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+    //scope.selected = (2,4);
+
+    //$scope.getClass = function(ind){
+    //    if( ind === $scope. ){
+    //        return "selected";
+    //    } else{
+    //        return "";
+    //    }
+    //}
+}
